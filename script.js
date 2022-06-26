e5deb2f5598af0325e52a35dabbda4f55dc30653
Alan <alanvan3791@gmail.com>
2022-06-26 00:08:55 -0300

    final commit

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const input = document.getElementById("text-area");
+const button = document.getElementById("btn");
 
 input.addEventListener('keyup',autoResize);
 input.addEventListener('keydown',autoResize);
+button.addEventListener('click',guess)
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+
+function guess(){
+    let counter = 0;
+    for (let i = 0; i < input.value.length; i++) {
+        for (let n = 0; n < vowels.length; n++) {
+            if(input.value[i]===vowels[n]){
+                counter +=1;
+            }        
+        }
+    }
+
+    alert("The amount of vowels is: " + counter);
+}
 
 function autoResize()
 {
     input.style.height = "0px";
     let scheight = input.scrollHeight;
     input.style.height = scheight+"px";
-    console.log(scheight);
-    console.log(input.style.height);
 }
